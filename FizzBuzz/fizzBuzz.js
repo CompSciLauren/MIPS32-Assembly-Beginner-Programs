cd99850c4eb756d3ad7efd0835b4c8a6160335ff
Lauren Stephenson <compscilauren@gmail.com>
2020-04-18 16:53:30 -0500

    Add FizzBuzz

---
+++ FizzBuzz/fizzBuzz.js
@@ -0,0 +1,24 @@
+let result = '';
+for (let i = 1; i <= 100; i++) {
+  if (i == 1) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      result += 'Fizz Buzz';
+    } else if (i % 3 == 0) {
+      result += 'Fizz';
+    } else if (i % 5 == 0) {
+      result += 'Buzz';
+    } else {
+      result += i;
+    }
+  } else if (i % 3 == 0 && i % 5 == 0) {
+    result += ', Fizz Buzz';
+  } else if (i % 3 == 0) {
+    result += ', Fizz';
+  } else if (i % 5 == 0) {
+    result += ', Buzz';
+  } else {
+    result += ', ' + i;
+  }
+}
+
+console.log(result);
