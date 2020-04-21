a7bf49655e66e99ea5daba91f9f13011e97fa5a2
Lauren Stephenson <compscilauren@gmail.com>
2020-04-20 20:24:20 -0500

    Add string variables

--- FizzBuzz/fizzBuzz.asm
+++ FizzBuzz/fizzBuzz.asm
@@ -1,10 +1,10 @@
 .data
-out_string:    .asciiz    "\nHello, World!\n"
+fizz_string:    .asciiz    ", Fizz"
+buzz_string:    .asciiz    ", Buzz"
+fizzbuzz_string:    .asciiz    ", Fizz Buzz"
 
 .text
 .globl  main
-
-# out_string:    .asciiz    "\nHello, World!\n"
 
 main:
     li $v0, 4
@@ -15,9 +15,23 @@ loop:
     beq $t1, $t0, end # if t1 == t0 end loop
     add $t2, $t2, 1 # body
     addi $t1, $t1, 1 # add 1 to t1
-    la $a0, out_string # print "Hello, World!"
+    j fizz
+    j loop
+fizz:
+    la $a0, fizz_string
+    li $v0, 4
     syscall
-    j loop # jump back to the top
+    j loop
+buzz:
+    la $a0, buzz_string
+    li $v0, 4
+    syscall
+    j loop
+fizzbuzz:
+    la $a0, fizzbuzz_string
+    li $v0, 4
+    syscall
+    j loop
 end:
     li $v0, 10
     syscall
